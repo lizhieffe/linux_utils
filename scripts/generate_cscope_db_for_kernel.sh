2ae804f5585ebd221cfe559eaeaa25271bd90510
Zhi Li <lizhieffe@gmail.com>
2018-04-25 00:49:22 -0700

    add more scripts

--- scripts/generate_cscope_db_for_kernel.sh
+++ scripts/generate_cscope_db_for_kernel.sh
@@ -3,13 +3,19 @@
 # Inspired by doc: http://cscope.sourceforge.net/large_projects.html
 
 LNX="$HOME"/development/linux
-cd "$HOME"
-find  $LNX                                                                \
+cd "$LNX"
+
+if [[ ! -e "$LNX"/cscope.files ]]; then
+  touch "$LNX"/cscope.files
+fi
+
+find  "$LNX"                                                             \
   -path "$LNX/arch/*" ! -path "$LNX/arch/i386*" -prune -o               \
   -path "$LNX/include/asm-*" ! -path "$LNX/include/asm-i386*" -prune -o \
   -path "$LNX/tmp*" -prune -o                                           \
   -path "$LNX/Documentation*" -prune -o                                 \
   -path "$LNX/scripts*" -prune -o                                       \
   -path "$LNX/drivers*" -prune -o                                       \
-  -name "*.[chxsS]" -print >"LNX"/cscope.files
+  -name "*.[chxsS]" -print >"$LNX"/cscope.files
+
 cscope -b -q -k
