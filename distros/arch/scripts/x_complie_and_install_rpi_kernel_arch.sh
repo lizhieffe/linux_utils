09da35466ac3d9c5a4a04c422f521c17a1911c1e
lizhi <lizhieffe@gmail.com>
2018-05-25 23:03:09 -0700

    update script

--- distros/arch/scripts/x_complie_and_install_rpi_kernel_arch.sh
+++ distros/arch/scripts/x_complie_and_install_rpi_kernel_arch.sh
@@ -13,12 +13,44 @@
 # Exit on error.
 set -e
 
+USAGE_VERBOSE="Usage: cmd [-a] [-d]"
+
+while getopts ":ad" opt; do
+  case ${opt} in
+    a ) # compile entire kernel
+      COMPILE_ALL=true
+      ;;
+    d ) # compile device tree only
+      COMPILE_DT_ONLY=true
+      ;;
+    \? ) echo "$USAGE_VERBOSE"
+      exit
+      ;;
+  esac
+done
+if [ $OPTIND -eq 1 ]; then
+  echo "No option were passed"
+  echo "$USAGE_VERBOSE"
+  exit
+fi
+shift $((OPTIND -1))
+echo "$# non-option arguments"
+
+if [ "$COMPILE_ALL" = true ]; then
+  TARGET='zImage modules dtbs'
+  TARGET_VERBOSE_NAME="Kernel"
+else
+  TARGET="dtbs"
+  TARGET_VERBOSE_NAME="Device Tree (DT)"
+fi
+echo "Will compile and install $TARGET_VERBOSE_NAME"
+
 KERNEL=kernel7
 KERNEL_DIR="$HOME/development/rpi_linux"
 CONFIG_FILE=".config"
 CORES=`getconf _NPROCESSORS_ONLN`
 SD_CARD=sdd
-MOUNT_DIR="/tmp/rpi_mnt/"
+MOUNT_DIR="/tmp/rpi_mnt"
 
 if [ -e "/dev/${SD_CARD}1" ] && [ -e "/dev/${SD_CARD}2" ] && [ -e "/dev/${SD_CARD}5" ] && [ -e "/dev/${SD_CARD}6" ] && [ -e "/dev/${SD_CARD}7" ]; then
   echo "Find SD card on $SD_CARD"
@@ -43,8 +75,12 @@ fi
 echo "Generating the kernel config..."
 sudo make -j "$CORES" ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2709_defconfig
 
-echo "Compiling the kernel with name ${KERNEL} using ${CORES} cores..."
-sudo make -j "$CORES" ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs
+if [ "$COMPILE_ALL" = true ]; then
+  echo "Compiling the "$TARGET_VERBOSE_NAME" with name ${KERNEL} using ${CORES} cores..."
+else
+  echo "Compiling the "$TARGET_VERBOSE_NAME" using ${CORES} cores..."
+fi
+sudo make -j "$CORES" ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- $TARGET
 
 if [ -d "$MOUNT_DIR" ]; then
   rm -rf "$MOUNT_DIR"
@@ -55,12 +91,16 @@ mkdir "$MOUNT_DIR"/ext4
 sudo mount /dev/"$SD_CARD"6 "$MOUNT_DIR"/fat32
 sudo mount /dev/"$SD_CARD"7 "$MOUNT_DIR"/ext4
 
-echo "Compiling and installing the modules with using ${CORES} cores..."
-sudo make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH="$MOUNT_DIR"/ext4 modules_install
+if [ "$COMPILE_ALL" = true ]; then
+  echo "Compiling and installing the modules with using ${CORES} cores..."
+  sudo make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH="$MOUNT_DIR"/ext4 modules_install
+fi
 
-echo "Copying the kernel to SD card $SD_CARD"
-sudo cp "$MOUNT_DIR"/fat32/$KERNEL.img "$MOUNT_DIR"/fat32/$KERNEL-backup.img
-sudo cp arch/arm/boot/zImage "$MOUNT_DIR"/fat32/$KERNEL.img
+echo "Copying to SD card $SD_CARD"
+if [ "$COMPILE_ALL" = true ]; then
+  sudo cp "$MOUNT_DIR"/fat32/$KERNEL.img "$MOUNT_DIR"/fat32/$KERNEL-backup.img
+  sudo cp arch/arm/boot/zImage "$MOUNT_DIR"/fat32/$KERNEL.img
+fi
 sudo cp arch/arm/boot/dts/*.dtb "$MOUNT_DIR"/fat32/
 sudo cp arch/arm/boot/dts/overlays/*.dtb* "$MOUNT_DIR"/fat32/overlays/
 sudo cp arch/arm/boot/dts/overlays/README "$MOUNT_DIR"/fat32/overlays/
@@ -70,4 +110,4 @@ sudo umount "$MOUNT_DIR"/ext4
 echo "Ejecting the SD card $SD_CARD"
 udisksctl power-off -b /dev/"$SD_CARD"
 
-echo "The kernel is built and installed to SD card $SD_CARD successfully. Please plug it in your RPI device and restart."
+echo "The $TARGET_VERBOSE_NAME is built and installed to SD card $SD_CARD successfully. Please plug it in your RPI device and restart."
