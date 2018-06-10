78945151f2fabacdea36f440648a75596082ade2
lizhi <lizhieffe@gmail.com>
2018-06-10 04:56:19 +0000

    update script

--- distros/arch/scripts/install_aur_arch.sh
+++ distros/arch/scripts/install_aur_arch.sh
@@ -1,6 +1,13 @@
 #/bin/sh
 
-cd ~/sources
+BASE_DIR="~/source"
+# Create dir if not exist.
+mkdir -p "$BASE_DIR"
+
+cd "$BASE_DIR" 
 git clone https://aur.archlinux.org/"$1".git
 cd "$1"
 makepkg -sic
+
+cd "$BASE_DIR" 
+rm -rf "$1"
